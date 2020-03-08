0a193fbf6af66024271cd5ae686f6793dd5abf77
Robert Martin <robertmartinbc@gmail.com>
2020-03-08 14:41:25 -0700

    Set up videos page.

--- src/app/videos/videos.component.ts
+++ src/app/videos/videos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-videos',
@@ -6,6 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./videos.component.scss']
 })
 export class VideosComponent implements OnInit {
+  customOptions: OwlOptions = {
+    loop: true,
+    margin: 10,
+    dots: false,
+    navSpeed: 700,
+    responsive: {
+      0: {
+        items: 1
+      },
+      500: {
+        items: 2
+      },
+      650: {
+        items: 3
+      },
+      1000: {
+        items: 4
+      },
+      1200: {
+        items: 5
+      }
+    },
+    nav: true
+  }
 
   constructor() { }
 
