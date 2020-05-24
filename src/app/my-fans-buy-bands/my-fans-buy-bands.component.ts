7d780e27ae8d9a153d8e089f7ab4839bd002268a
Robert Martin <robertmartinbc@gmail.com>
2020-05-24 16:48:38 -0700

    Fiunished setting up Fan pages with no data.

--- src/app/my-fans-buy-bands/my-fans-buy-bands.component.ts
+++ src/app/my-fans-buy-bands/my-fans-buy-bands.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { BandsbyfansService } from '../shared/services/bandsbyfans.service';
+import { Router, Params } from '@angular/router';
+import { AuthService } from '../shared/services/auth.service';
+import { AngularFireStorage, AngularFireStorageReference } from 'angularfire2/storage';
+
 
 @Component({
   selector: 'app-my-fans-buy-bands',
@@ -6,10 +11,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-fans-buy-bands.component.scss']
 })
 export class MyFansBuyBandsComponent implements OnInit {
+  ref: AngularFireStorageReference;
+  items: Array<any>;
+  hideWhenNoStudent: boolean = false; //Hide albums table if no albums created.
+  noData: boolean = false;
+  preLoader: boolean = true;
+  //albumPic: string = '/assets/images/no-avatar.gif';
 
-  constructor() { }
+  constructor(
+    private bandsbyfansService: BandsbyfansService,
+    public router: Router,
+    private afAuth: AuthService,
+    private afStorage: AngularFireStorage
+  ) { }
 
   ngOnInit() {
+    this.getData();
+    this.dataState();
+  }
+
+  getData() {
+    this.bandsbyfansService.getBandsByFans()
+    .subscribe(result => {
+      this.items = result;
+    })
+  }
+
+  dataState() {
+    this.bandsbyfansService.getBandsByFans().subscribe(data => {
+      this.preLoader = false;
+      if(data.length <= 0){
+        this.hideWhenNoStudent = false;
+        this.noData = true;
+      } else {
+        this.hideWhenNoStudent = true;
+        this.noData = false;
+      }
+    })
   }
 
 }
--- src/app/my-fans-cdfunds/my-fans-cdfunds.component.ts
+++ src/app/my-fans-cdfunds/my-fans-cdfunds.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CdfundsService } from '../shared/services/cdfunds.service';
+import { Router, Params } from '@angular/router';
+import { AuthService } from '../shared/services/auth.service';
+import { AngularFireStorage, AngularFireStorageReference } from 'angularfire2/storage';
 
 @Component({
   selector: 'app-my-fans-cdfunds',
@@ -6,10 +10,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-fans-cdfunds.component.scss']
 })
 export class MyFansCDFundsComponent implements OnInit {
+  ref: AngularFireStorageReference;
+  items: Array<any>;
+  hideWhenNoStudent: boolean = false; //Hide albums table if no albums created.
+  noData: boolean = false;
+  preLoader: boolean = true;
+  //albumPic: string = '/assets/images/no-avatar.gif';
 
-  constructor() { }
+
+constructor(
+  private cdfundsService: CdfundsService,
+  public router: Router,
+  private afAuth: AuthService,
+  private afStorage: AngularFireStorage
+) { }
 
   ngOnInit() {
+    this.getData();
+    this.dataState();
+  }
+
+  getData() {
+    this.cdfundsService.getCdFunds()
+    .subscribe(result => {
+      this.items = result;
+    })
+  }
+
+  dataState() {
+    this.cdfundsService.getCdFunds().subscribe(data => {
+      this.preLoader = false;
+      if(data.length <= 0){
+        this.hideWhenNoStudent = false;
+        this.noData = true;
+      } else {
+        this.hideWhenNoStudent = true;
+        this.noData = false;
+      }
+    })
   }
 
 }
