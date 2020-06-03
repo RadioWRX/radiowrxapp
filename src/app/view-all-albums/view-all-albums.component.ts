4a70b0b9212e44c3567c52ba76b0d737a269d143
Robert Martin <robertmartinbc@gmail.com>
2020-06-02 18:13:03 -0700

    Linked uo data to view all bands and events.

--- src/app/view-all-albums/view-all-albums.component.ts
+++ src/app/view-all-albums/view-all-albums.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+import { AlbumService } from '../shared/services/album.service';
+import { Router, Params } from '@angular/router';
+import { AngularFireStorage, AngularFireStorageReference } from 'angularfire2/storage';
 
 @Component({
   selector: 'app-view-all-albums',
@@ -7,9 +11,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewAllAlbumsComponent implements OnInit {
 
-  constructor() { }
+  albumItems: Array<any>;
+  albumPic: string = '/assets/images/no-avatar.gif';
+
+  // Custom options for the Owl Carousel
+  customOptions: OwlOptions = {
+    loop: true,
+    margin: 10,
+    dots: false,
+    navSpeed: 700,
+    responsive: {
+      0: {
+        items: 1
+      },
+      500: {
+        items: 2
+      },
+      650: {
+        items: 3
+      },
+      1000: {
+        items: 4
+      },
+      1200: {
+        items: 5
+      }
+    },
+    nav: true
+  }
+
+  constructor(
+    private albumService: AlbumService,
+    public router: Router,
+    private afStorage: AngularFireStorage
+    ) { }
 
   ngOnInit() {
+    this.getAlbumsData();
   }
+
+  //FIX: Work out how all users albums can be displayed.
+  getAlbumsData() {
+    this.albumService.getDummyAlbums()
+    .subscribe(result => {
+      this.albumItems = result;
+      console.log("AlbumItems ", this.albumItems);
+    })
+  }
+
+  /*viewAlbum(item, docId) {
+    this.router.navigate(['/fan-view-album-details/' + item.payload.doc.id]);
+    docId = item.payload.doc.id;
+    localStorage.setItem('docId', docId);
+  }*/
 
 }
--- src/app/view-all-events/view-all-events.component.ts
+++ src/app/view-all-events/view-all-events.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+import { EventsService } from '../shared/services/events.service';
+import { Router, Params } from '@angular/router';
+import { AngularFireStorage, AngularFireStorageReference } from 'angularfire2/storage';
 
 @Component({
   selector: 'app-view-all-events',
@@ -7,9 +11,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewAllEventsComponent implements OnInit {
 
-  constructor() { }
+  eventItems: Array<any>;
+  eventPic: string = '/assets/images/no-avatar.gif';
+
+  // Custom options for the Owl Carousel
+  customOptions: OwlOptions = {
+    loop: true,
+    margin: 10,
+    dots: false,
+    navSpeed: 700,
+    responsive: {
+      0: {
+        items: 1
+      },
+      500: {
+        items: 2
+      },
+      650: {
+        items: 3
+      },
+      1000: {
+        items: 4
+      },
+      1200: {
+        items: 5
+      }
+    },
+    nav: true
+  }
+
+  constructor(
+    private eventsService: EventsService,
+    public router: Router,
+    private afStorage: AngularFireStorage
+    ) { }
 
   ngOnInit() {
+    this.getEventsData()
+  }
+
+  //FIX: Work out how all users albums can be displayed.
+  getEventsData() {
+    this.eventsService.getDummyEvents()
+    .subscribe(result => {
+      this.eventItems = result;
+      console.log("EventItems ", this.eventItems);
+    })
+    /*this.albumService.getAllAlbums()
+    .subscribe(result => {
+      this.items = result;
+      console.log("Items are " + this.items);
+    })*/
   }
 
 }
