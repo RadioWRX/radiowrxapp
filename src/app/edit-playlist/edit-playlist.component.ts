3991f1c83269e12d59f0d00c5992e88b0175872e
Robert Martin <robertmartinbc@gmail.com>
2020-06-25 19:42:45 -0700

    CRUD for creating a playist completed.

--- src/app/edit-playlist/edit-playlist.component.ts
+++ src/app/edit-playlist/edit-playlist.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { PlaylistService } from '../shared/services/playlist.service';
+import { Router } from '@angular/router';
+import { AuthService } from '../shared/services/auth.service';
+
 
 @Component({
   selector: 'app-edit-playlist',
@@ -6,10 +12,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-playlist.component.scss']
 })
 export class EditPlaylistComponent implements OnInit {
+  editForm: FormGroup;
+  item: any;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private playlistService: PlaylistService,
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private afAuth: AuthService
+  ) { }
 
   ngOnInit() {
+    this.route.data.subscribe(routeData => {
+      let data = routeData['data'];
+      if (data) {
+        this.item = data.payload.data();
+        this.item.id = data.payload.id;
+        this.editPlaylistForm();
+      }
+    })
   }
 
+  editPlaylistForm() {
+    this.editForm = this.fb.group({
+      playlistTitle: [this.item.playlistTitle, Validators.required],
+      description: [this.item.description, Validators.required]
+    })
+  }
+
+  onSubmit(value){
+    this.playlistService.updatePlaylist(this.item.id, value)
+    .then(
+      res => {
+        this.router.navigate(['/my-fans-music']);
+      }
+    )
+  }
+
+  cancel() {
+    this.router.navigate(['/my-fans-music']);
+  }
 }
